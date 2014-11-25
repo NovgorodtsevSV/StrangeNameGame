3b3b0bbf2404debcd6a5d229209529519154107e
BoBaH6eToH <freezedunk@gmail.com>
2014-11-25 16:06:27 +0500

    first commit

---
+++ src/com/StrangeNameGame/MainACtivity.java
@@ -0,0 +1,16 @@
+package com.StrangeNameGame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainACtivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        int a = 0;
+    }
+}
